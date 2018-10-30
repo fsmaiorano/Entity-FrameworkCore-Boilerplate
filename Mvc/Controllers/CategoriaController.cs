15114ada6b0078f0029acace1755a2fc720ed90b
Fábio <fsmaiorano@gmail.com>
2018-10-30 20:24:20 -0300

    add: first save on database with async

--- Mvc/Controllers/CategoriaController.cs
+++ Mvc/Controllers/CategoriaController.cs
@@ -1,11 +1,30 @@
+using System.Threading.Tasks;
+using Dados;
+using Dominio.Entidades;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mvc.Controllers
 {
     public class CategoriaController : Controller
     {
+        private readonly ApplicationDbContext _contexto;
+
+        public CategoriaController(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
         public IActionResult Salvar()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Salvar(Categoria categoria)
+        {
+            _contexto.Categorias.Add(categoria);
+            await _contexto.SaveChangesAsync();
             return View();
         }
     }
